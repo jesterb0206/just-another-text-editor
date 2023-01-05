b1ae608e5e20c7ded90b06b5b5a5d52d940f3063
jesterb0206 <jesterb@seattleu.edu>
2023-01-05 14:42:55 -0800

    Prettier Configuration

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,37 +1,37 @@
-import {openDB} from "idb";
+import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB("jate", 1, {
+  openDB('jate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains("jate")) {
-        console.log("jate database already exists");
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
         return;
       }
-      db.createObjectStore("jate", {keyPath: "id", autoIncrement: true});
-      console.log("jate database created");
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
     },
   });
 
 // Accepts content and adds it to the database
 
 export const putDb = async (content) => {
-  const jateDb = await openDB("jate", 1);
-  const tx = jateDb.transaction("jate", "readwrite");
-  const store = tx.objectStore("jate");
-  const request = store.put({value: content});
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ value: content });
   const result = await request;
-  console.log("The data was successfully saved to the database!", result);
+  console.log('The data was successfully saved to the database!', result);
 };
 
 // Retrieves the database's content
 
 export const getDb = async () => {
-  const jateDb = await openDB("jate", 1);
-  const tx = jateDb.transaction("jate", "readonly");
-  const store = tx.objectStore("jate");
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
-  console.log("result.value", result);
+  console.log('result.value', result);
   return result;
 };
 
