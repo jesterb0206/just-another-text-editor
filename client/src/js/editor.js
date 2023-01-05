627662adac1e2bbd1bc5b94884d9e4a1d4b0a22c
jesterb0206 <jesterb@seattleu.edu>
2023-01-05 14:50:06 -0800

    Slight tweak

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -22,18 +22,18 @@ export default class {
       tabSize: 2,
     });
 
-    // When the editor is ready, set the value to whatever is stored in indexeddb.
-    // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
+    // When the editor is ready, set the value to whatever is stored in indexedDb.
+    // Fall back to localStorage if nothing is stored in indexedDb, and if neither is available, set the value to header.
     getDb().then((data) => {
       console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
+      this.editor.setValue(data[0]?.content || localData || header);
     });
 
     this.editor.on('change', () => {
       localStorage.setItem('content', this.editor.getValue());
     });
 
-    // Save the content of the editor when the editor itself is loses focus
+    // Save the content of the editor when the editor itself loses focus
     this.editor.on('blur', () => {
       console.log('The editor has lost focus');
       putDb(localStorage.getItem('content'));
