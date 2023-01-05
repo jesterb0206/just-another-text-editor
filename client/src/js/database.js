41e304ed7f47df808afe34afc75ccc1f7ec2215d
jesterb0206 <jesterb@seattleu.edu>
2023-01-05 14:38:28 -0800

    Retrieve database content

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -23,7 +23,16 @@ export const putDb = async (content) => {
   console.log("The data was successfully saved to the database!", result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error("getDb not implemented");
+// Retrieves the database's content
+
+export const getDb = async () => {
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  console.log("result.value", result);
+  return result;
+};
 
 initdb();
