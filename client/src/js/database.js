735cb51772d47615e416b3b0ef4dadb383be1a0b
jesterb0206 <jesterb@seattleu.edu>
2023-01-05 14:28:09 -0800

    Accept content and add it to the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,29 @@
-import { openDB } from 'idb';
+import {openDB} from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", {keyPath: "id", autoIncrement: true});
+      console.log("jate database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Accepts content and adds it to the database
+
+export const putDb = async (content) => {
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({value: content});
+  const result = await request;
+  console.log("The data was successfully saved to the database!", result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => console.error("getDb not implemented");
 
 initdb();
