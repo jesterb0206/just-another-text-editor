f2019ce11c81b6b361f999da1682015436539042
jesterb0206 <jesterb@seattleu.edu>
2023-01-05 16:21:33 -0800

    Added CSS loaders and babel to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -63,7 +63,26 @@ module.exports = () => {
     // Added CSS loaders and babel to webpack
 
     module: {
-      rules: [],
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: [
+                '@babel/plugin-proposal-object-rest-spread',
+                '@babel/transform-runtime',
+              ],
+            },
+          },
+        },
+      ],
     },
   };
 };
