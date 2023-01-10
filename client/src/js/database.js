ae8989e7513a02820aa5c0d66cb6256d36cef1bd
jesterb0206 <jesterb@seattleu.edu>
2023-01-09 17:18:01 -0800

    IDB Should Now Load And Save Data Correctly

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,7 +18,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ value: content });
+  const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('The data was successfully saved to the database!', result);
 };
@@ -26,13 +26,16 @@ export const putDb = async (content) => {
 // Retrieves the database's content
 
 export const getDb = async () => {
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
-  const request = store.getAll();
-  const result = await request;
-  console.log('result.value', result);
-  return result;
+  try {
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.get(1);
+    const result = await request;
+    return result?.value;
+  } catch {
+    console.error('getDb still needs to be implemented!');
+  }
 };
 
 initdb();
